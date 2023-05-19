9a271c5df52c5df02bb508aadb9ad124fbbf7226
thinkAfCod <q315xia@163.com>
2023-05-19 15:37:29 +0800

    feat: metrics

---
+++ hildr-node/src/main/java/io/optimism/telemetry/InnerMetrics.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2023 281165273grape@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.optimism.telemetry;
+
+import com.sun.net.httpserver.HttpServer;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * InnerMetrics tools.
+ *
+ * @author thinkAfCod
+ * @since 2023.05
+ */
+public class InnerMetrics {
+
+  private static AtomicReference<BigInteger> FINALIZED_HEAD;
+  private static AtomicReference<BigInteger> SAFE_HEAD;
+  private static AtomicReference<BigInteger> SYNCED;
+  private static PrometheusMeterRegistry registry;
+  private static HttpServer httpServer;
+
+  static {
+    FINALIZED_HEAD = new AtomicReference<>();
+    SAFE_HEAD = new AtomicReference<>();
+    SYNCED = new AtomicReference<>();
+    registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    Gauge.builder("finalized_head", FINALIZED_HEAD, ref -> ref.get().doubleValue())
+        .description("finalized head number")
+        .register(registry);
+
+    Gauge.builder("safe_head", SAFE_HEAD, ref -> ref.get().doubleValue())
+        .description("safe head number")
+        .register(registry);
+
+    Gauge.builder("synced", SYNCED, ref -> ref.get().doubleValue())
+        .description("synced flag")
+        .register(registry);
+
+    Metrics.addRegistry(registry);
+  }
+
+  private InnerMetrics() {}
+
+  /**
+   * start a http server for prometheus to access.
+   *
+   * @param port custom http server port
+   */
+  public static void start(int port) {
+    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+      httpServer = HttpServer.create(new InetSocketAddress(port), 2);
+      httpServer.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+      httpServer.createContext(
+          "/prometheus",
+          httpExchange -> {
+            String response = registry.scrape();
+            httpExchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = httpExchange.getResponseBody()) {
+              os.write(response.getBytes());
+            }
+          });
+      executor.submit(httpServer::start);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /** stop the http server. */
+  public static void stop() {
+    if (httpServer != null) {
+      httpServer.stop(5);
+    }
+  }
+
+  /**
+   * set metrics finalized head block.
+   *
+   * @param finalizedHead finalized head block
+   */
+  public static void setFinalizedHead(BigInteger finalizedHead) {
+    FINALIZED_HEAD.compareAndSet(FINALIZED_HEAD.get(), finalizedHead);
+  }
+
+  /**
+   * set metrics safe head block.
+   *
+   * @param safeHead safe head block
+   */
+  public static void setSafeHead(BigInteger safeHead) {
+    SAFE_HEAD.compareAndSet(SAFE_HEAD.get(), safeHead);
+  }
+
+  /**
+   * set metrics synced block count.
+   *
+   * @param synced synced block count
+   */
+  public static void setSynced(BigInteger synced) {
+    SYNCED.compareAndSet(SYNCED.get(), synced);
+  }
+}
---
+++ hildr-node/src/test/java/io/optimism/telemetry/InnerMetricsTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023 281165273grape@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.optimism.telemetry;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Inner metrics test case.
+ *
+ * @author thinkAfCod
+ * @since 2023.05
+ */
+public class InnerMetricsTest {
+
+  @BeforeAll
+  static void setUp() {
+    InnerMetrics.start(9200);
+  }
+
+  @AfterAll
+  static void tearDown() {
+    InnerMetrics.stop();
+  }
+
+  private String getMetric() throws IOException {
+    OkHttpClient client = new OkHttpClient.Builder().build();
+    Request request = new Request.Builder().get().url("http://127.0.0.1:9200/prometheus").build();
+    return client.newCall(request).execute().body().string();
+  }
+
+  @Test
+  void testUpdateMetric() throws IOException {
+    InnerMetrics.setFinalizedHead(BigInteger.ONE);
+    InnerMetrics.setSafeHead(BigInteger.TWO);
+    InnerMetrics.setSynced(BigInteger.valueOf(3L));
+    String metric = this.getMetric();
+    assertTrue(metric.contains("finalized_head 1.0"));
+    assertTrue(metric.contains("safe_head 2.0"));
+    assertTrue(metric.contains("synced 3.0"));
+  }
+}
