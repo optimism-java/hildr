64ee10baacb608df28ea4c234a223637dd202762
thinkAfCod <q315xia@163.com>
2023-07-31 16:07:05 +0800

    create hildr-batcher project

---
+++ hildr-batcher/src/main/java/io/optimism/batcher/compressor/RatioCompressor.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2023 q315xia@163.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.optimism.batcher.compressor;
+
+import java.io.IOException;
+import java.nio.CharBuffer;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * RatioCompressor class.
+ *
+ * @author thinkAfCod
+ * @since 0.1.1
+ */
+public class RatioCompressor implements Compressor {
+
+  RatioCompressor() {
+    // todo plan to use java.util.zip.Deflater
+  }
+
+  @Override
+  public int write(byte[] p) {
+    return 0;
+  }
+
+  @Override
+  public int read(byte[] p) {
+    return 0;
+  }
+
+  @Override
+  public int read(@NotNull CharBuffer cb) throws IOException {
+    return 0;
+  }
+
+  @Override
+  public void reset() {}
+
+  @Override
+  public int length() {
+    return 0;
+  }
+
+  @Override
+  public void fullErr() {}
+
+  @Override
+  public void close() throws IOException {}
+
+  @Override
+  public void flush() throws IOException {}
+}
---
+++ hildr-batcher/src/main/java/io/optimism/batcher/compressor/ex/CompressorFullException.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2023 q315xia@163.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.optimism.batcher.compressor.ex;
+
+/**
+ * If the compressor is full and no more data should be written or the compressor is known to be
+ * full.
+ *
+ * @author thinkAfCod
+ * @since 0.1.1
+ */
+public class CompressorFullException extends RuntimeException {
+
+  /**
+   * Constructor of CompressorFullException.
+   *
+   * @param message error message
+   */
+  public CompressorFullException(String message) {
+    super(message);
+  }
+}
