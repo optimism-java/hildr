04912317d6644af876ca69590e25858f250ed5a6
thinkAfCod <q315xia@163.com>
2023-08-30 18:39:04 +0800

    batcher metrics server

---
+++ hildr-batcher/src/main/java/io/optimism/batcher/telemetry/BatcherMetricsServer.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 q315xia@163.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.optimism.batcher.telemetry;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.optimism.utilities.telemetry.MetricsServer;
+
+/**
+ * The BatcherMetricsServer type.
+ *
+ * @author thinkAfCod
+ * @since 0.1.1
+ */
+public class BatcherMetricsServer {
+
+  private BatcherMetricsServer() {}
+
+  /**
+   * Create a NoopMatcherMetrics instance.
+   *
+   * @return NoopMatcherMetrics instance.
+   */
+  public static BatcherMetrics noop() {
+    return new NoopBatcherMetrics();
+  }
+
+  /**
+   * Start a metrics server on specific port.
+   *
+   * @param port Server port
+   */
+  public static void start(int port) {
+    MeterRegistry registry = getRegistry();
+    MetricsServer.start(registry, port);
+  }
+
+  /**
+   * Get single instance of the meter register .
+   *
+   * @return the meter register
+   */
+  public static MeterRegistry getRegistry() {
+    return BatcherMetricsRegistry.INSTANCE.registry();
+  }
+
+  /** Stop an active metrics server. */
+  public static void stop() {
+    MetricsServer.stop();
+  }
+
+  /**
+   * Check if the service is active.
+   *
+   * @return Return ture if MetricsServer has been started and still alive, otherwise false.
+   */
+  public static boolean isActive() {
+    return MetricsServer.isActive();
+  }
+}
---
+++ hildr-batcher/src/main/java/io/optimism/batcher/telemetry/NoopBatcherMetrics.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2023 q315xia@163.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.optimism.batcher.telemetry;
+
+import io.optimism.type.L1BlockRef;
+import io.optimism.type.L2BlockRef;
+import io.optimism.utilities.derive.stages.Frame;
+import org.web3j.protocol.core.methods.response.EthBlock;
+
+/**
+ * An empty metrics when not enable open a metrics.
+ *
+ * @author thinkAfCod
+ * @since 0.1.1
+ */
+public class NoopBatcherMetrics implements BatcherMetrics {
+
+  /** The NoopBatcherMetrics constructor. */
+  public NoopBatcherMetrics() {}
+
+  @Override
+  public void recordLatestL1Block(L1BlockRef l1ref) {}
+
+  @Override
+  public void recordL2BlocksLoaded(L2BlockRef l2ref) {}
+
+  @Override
+  public void recordChannelOpened(Frame frame, int numPendingBlocks) {}
+
+  @Override
+  public void recordL2BlocksAdded(
+      L2BlockRef l2ref,
+      int numBlocksAdded,
+      int numPendingBlocks,
+      int inputBytes,
+      int outputComprBytes) {}
+
+  @Override
+  public void recordL2BlockInPendingQueue(EthBlock.Block block) {}
+
+  @Override
+  public void recordL2BlockInChannel(EthBlock.Block block) {}
+
+  @Override
+  public void recordChannelClosed(
+      Frame frame,
+      int numPendingBlocks,
+      int numFrames,
+      int inputBytes,
+      int outputComprBytes,
+      String errorReason) {}
+
+  @Override
+  public void recordChannelFullySubmitted(Frame frame) {}
+
+  @Override
+  public void recordChannelTimedOut(Frame frame) {}
+
+  @Override
+  public void recordBatchTxSubmitted() {}
+
+  @Override
+  public void recordBatchTxSuccess() {}
+
+  @Override
+  public void recordBatchTxFailed() {}
+
+  @Override
+  public void recordInfo(String version) {}
+
+  @Override
+  public void recordUp() {}
+}
