2ad386c0534e314c9916a0794c733bf0252abe0b
thinkAfCod <q315xia@163.com>
2023-11-26 19:18:44 +0800

    fix: devnet

--- docker/start-hildr-node-java.sh
+++ docker/start-hildr-node-java.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -e
 
+DEVNET=""
+
+if [ $NETWORK = "custom" ] || [ $NETWORK = "devnet" ]
+then
+    NETWORK="./rollup.json"
+    if [ "$NETWORK" = "devnet" ]
+    then
+        DEVNET="--devnet"
+    fi
+fi
+
 if [ $SYNC_MODE = "full" ]
 then
     exec java --enable-preview \
@@ -12,6 +23,7 @@ then
         --l2-rpc-url http://${EXECUTION_CLIENT}:8545 \
         --l2-engine-url http://${EXECUTION_CLIENT}:8551 \
         --rpc-port $RPC_PORT \
+        $DEVNET \
         --sync-mode $SYNC_MODE
 elif [ $SYNC_MODE = "checkpoint"]
 then
@@ -24,6 +36,7 @@ then
         --l2-rpc-url http://${EXECUTION_CLIENT}:8545 \
         --l2-engine-url http://${EXECUTION_CLIENT}:8551 \
         --rpc-port $RPC_PORT \
+        $DEVNET \
         --sync-mode $SYNC_MODE \
         --checkpoint-sync-url $CHECKPOINT_SYNC_URL \
         --checkpoint-hash $CHECKPOINT_HASH
