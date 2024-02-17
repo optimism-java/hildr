4daf3543d2fb4b452e53acfe5873cc7c4f34ab1d
thinkAfCod <q315xia@163.com>
2024-02-17 22:20:22 +0800

    feat: beacon blob fetcher

---
+++ hildr-utilities/src/main/java/io/optimism/utilities/rpc/HttpClientProvider.java
@@ -0,0 +1,28 @@
+package io.optimism.utilities.rpc;
+
+import okhttp3.OkHttpClient;
+
+import java.time.Duration;
+
+/**
+ * Http client provider.
+ *
+ * @author thinkAfCod
+ * @since 0.2.6
+ */
+public class HttpClientProvider {
+
+    private HttpClientProvider() {}
+
+    /**
+     * Create a http client.
+     *
+     * @return a HttpClient
+     */
+    public static OkHttpClient create() {
+        return new OkHttpClient.Builder()
+                .addInterceptor(new RetryRateLimitInterceptor())
+                .build();
+    }
+
+}
