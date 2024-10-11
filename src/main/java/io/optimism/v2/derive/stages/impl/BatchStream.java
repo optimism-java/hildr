ab7d361bbd5b202cad00db0c2ec33d0df0ef713e
thinkAfCod <q315xia@163.com>
2024-10-12 00:46:50 +0800

    refactor: stages implement

---
+++ src/main/java/io/optimism/v2/derive/stages/impl/BatchStream.java
@@ -0,0 +1,30 @@
+package io.optimism.v2.derive.stages.impl;
+
+import io.optimism.v2.derive.stages.BatchQueueProvider;
+import io.optimism.v2.derive.stages.OriginAdvancer;
+import io.optimism.v2.derive.stages.OriginProvider;
+import io.optimism.v2.derive.stages.ResettableStage;
+import io.optimism.v2.derive.types.Batch;
+import io.optimism.v2.derive.types.BlockInfo;
+import io.optimism.v2.derive.types.SystemConfig;
+
+public class BatchStream implements BatchQueueProvider, OriginProvider, OriginAdvancer, ResettableStage {
+    @Override
+    public Batch nextBatch() {
+        return null;
+    }
+
+    @Override
+    public void flush() {}
+
+    @Override
+    public void advanceOrigin() {}
+
+    @Override
+    public BlockInfo origin() {
+        return null;
+    }
+
+    @Override
+    public void reset(BlockInfo base, SystemConfig config) {}
+}
