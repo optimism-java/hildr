155be90de01ec84082ae16245a197c6195576d00
thinkAfCod <q315xia@163.com>
2024-10-12 00:47:12 +0800

    refactor: pipeline declare

---
+++ src/main/java/io/optimism/v2/derive/pipeline/PipelineBuilder.java
@@ -0,0 +1,10 @@
+package io.optimism.v2.derive.pipeline;
+
+public class PipelineBuilder {
+
+    public PipelineBuilder() {}
+
+    public DerivationPipeline build() {
+        return new DerivationPipeline();
+    }
+}
